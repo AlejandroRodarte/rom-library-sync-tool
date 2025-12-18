ed2aeb1b944938a72e0fe57c1a99a1c05720b930
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-12-17 23:06:18 -0600

    add: group rom filenames by game title

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,43 @@
-import {} from "node:fs/promises";
+import { readdir } from "node:fs/promises";
 
-const main = async () => {};
+const dirPath = "/home/alejandro/Downloads/myrient/gamegear";
+
+const main = async () => {
+  // NOTE: output already sorts filenames in ascending order
+  const roms = await readdir(dirPath);
+
+  const groups: string[][] = [];
+  let previousTitle = "";
+
+  for (const rom of roms) {
+    // EXAMPLE
+    // rom: Super Space Invaders (USA, Europe).zip
+    // title: Super Space Invaders
+    const [title] = rom.split("(");
+
+    if (!title) {
+      console.error(`No title found for ROM ${rom}`);
+      continue;
+    }
+
+    // group ROMs by title
+    if (title === previousTitle) {
+      const lastGroup = groups[groups.length - 1];
+      if (lastGroup) lastGroup.push(rom);
+    } else {
+      const newGroup = [rom];
+      groups.push(newGroup);
+    }
+
+    previousTitle = title;
+  }
+
+  for (const group of groups) {
+    console.log("----------");
+    for (const rom of group) {
+      console.log(rom);
+    }
+  }
+};
 
 main();
