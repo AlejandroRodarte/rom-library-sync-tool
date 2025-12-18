c80a3bd3a03f90d8e36cb74925446b26e1df6521
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-12-18 14:04:34 -0600

    rename: rom/roms to filename/filenames

--- src/index.ts
+++ src/index.ts
@@ -14,28 +14,28 @@ const dirPath = "/home/alejandro/Downloads/myrient/gamegear";
 
 const main = async () => {
   // NOTE: output already sorts filenames in ascending order
-  const roms = await readdir(dirPath);
+  const filenames = await readdir(dirPath);
 
   const groups: Groups = {};
   let previousTitle = "";
 
-  for (const rom of roms) {
+  for (const filename of filenames) {
     // EXAMPLE
     // rom: Super Space Invaders (USA, Europe).zip
     // title: Super Space Invaders
-    const [title] = rom.split("(");
+    const [title] = filename.split("(");
 
     if (!title) {
-      console.error(`No title found for ROM ${rom}`);
+      console.error(`No title found for ROM ${filename}`);
       continue;
     }
 
     // group ROMs by title
     if (title === previousTitle) {
       const group = groups[title];
-      if (group) group.push({ filename: rom, labels: [], selected: false });
+      if (group) group.push({ filename, labels: [], selected: false });
     } else {
-      groups[title] = [{ filename: rom, labels: [], selected: false }];
+      groups[title] = [{ filename, labels: [], selected: false }];
     }
 
     previousTitle = title;
