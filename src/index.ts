c2d2174b62fa3c8ec1ec42decd2a7b4badba7f10
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-12-17 22:39:16 -0600

    delete: nuke previous main script

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,5 @@
-import { readdir, readFile } from "node:fs/promises";
-import path from "path";
-import { fileURLToPath } from "node:url";
+import {} from "node:fs/promises";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const CONSOLE_NAME = "snes";
-const localRomDirPath = `/media/alejandro/3tb-ssd/home/alejandro/games/roms/${CONSOLE_NAME}`;
-const myrientRomFilePath = path.join(
-  __dirname,
-  "..",
-  "data",
-  `rom-list-myrient-${CONSOLE_NAME}.txt`,
-);
-
-const main = async () => {
-  const myrientFileContent = await readFile(myrientRomFilePath, "utf-8");
-  const myrientRomNames = myrientFileContent
-    .split("\n")
-    .map((name) => name.split(".")[0]);
-
-  const localRomFiles = await readdir(localRomDirPath);
-  const localRomNames = localRomFiles.map((name) => name.split(".")[0]);
-
-  for (const myrientRom of myrientRomNames) {
-    let found = false;
-    for (const localRom of localRomNames) {
-      if (localRom === myrientRom) {
-        found = true;
-        break;
-      }
-    }
-    if (!found) {
-      console.log(myrientRom);
-    }
-  }
-};
+const main = async () => {};
 
 main();
