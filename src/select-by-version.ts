61e642c1194486a8818d4c9f854c352d791c4539
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-12-19 16:39:13 -0600

    refactor: improve selectByVersion logic (always select once)

--- src/select-by-version.ts
+++ src/select-by-version.ts
@@ -12,30 +12,32 @@ const selectByVersion = (
   let [highestVersionIndex, highestVersion]: [number, string] = [-1, ""];
 
   for (const [index, rom] of roms.entries()) {
+    let versionLabelFound = false;
+
     for (const label of rom.labels) {
+      if (versionLabelFound) break;
+
       if (label.match(versionFormat)) {
-        groupHasVersionFormat = true;
+        if (!groupHasVersionFormat) groupHasVersionFormat = true;
+        versionLabelFound = true;
 
         const result = compare(label, highestVersion);
 
         const firstVersion = highestVersionIndex === -1;
         const newHighestVersionFound = firstVersion ? false : result === 1;
 
-        if (newHighestVersionFound) {
-          const lowerVersionRom = roms[highestVersionIndex];
-          if (lowerVersionRom) lowerVersionRom.selected = false;
-        }
-
         if (firstVersion || newHighestVersionFound) {
           highestVersion = label;
           highestVersionIndex = index;
-          rom.selected = true;
         }
       }
     }
   }
 
-  if (groupHasVersionFormat) {
+  if (groupHasVersionFormat && highestVersionIndex !== -1) {
+    const highestVersionRom = roms[highestVersionIndex];
+    if (highestVersionRom) highestVersionRom.selected = true;
+
     for (const rom of roms) {
       let romHasVersionLabel = false;
       for (const label of rom.labels) {
