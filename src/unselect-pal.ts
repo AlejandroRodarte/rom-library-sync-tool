d4922fa77f5c3702caae314e741cc037e8d1de6c
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-12-20 13:49:51 -0600

    refactor: improve unselectPAL logic using array methods

--- src/unselect-pal.ts
+++ src/unselect-pal.ts
@@ -1,28 +1,10 @@
 import type { Rom } from "./types.js";
 
 const unselectPAL = (roms: Rom[]): void => {
-  let romHasNTSCLabel = false;
-
-  for (const rom of roms) {
-    for (const label of rom.labels) {
-      if (label.includes("NTSC")) {
-        romHasNTSCLabel = true;
-        break;
-      }
-    }
-    if (romHasNTSCLabel) break;
-  }
-
-  if (romHasNTSCLabel) {
-    for (const rom of roms) {
-      if (!rom.selected) continue;
-      for (const label of rom.labels) {
-        if (label.includes("PAL")) {
-          rom.selected = false;
-          break;
-        }
-      }
-    }
+  const ntscLabelFound = roms.some((rom) => rom.labels.some((label) => label.includes("NTSC")));
+  if (ntscLabelFound) {
+    const palRoms = roms.filter((rom) => rom.labels.some((label) => label.includes("PAL")));
+    palRoms.forEach((rom) => rom.selected = false);
   }
 };
 
