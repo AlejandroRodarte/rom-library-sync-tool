8ec928f5e459ba1b8ac0c0629c9ae1a168d80157
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-12-20 13:24:53 -0600

    refactor: condense unselectByCountry logic using array methods

--- src/unselect-by-country.ts
+++ src/unselect-by-country.ts
@@ -8,36 +8,12 @@ const unselectByCountry = (
   // short-circuit operation (for example, when a label for another country has already been found before the current one)
   if (shortCircuit) return shortCircuit;
 
-  let groupHasCountryLabel = false;
+  const countryLabelFound = roms.some((rom) => rom.labels.some((label) => label.includes(country)));
+  if (!countryLabelFound) return countryLabelFound;
 
-  // if country label is found in any of the ROMs, short-circuit
-  for (const rom of roms) {
-    for (const label of rom.labels) {
-      if (label.includes(country)) {
-        groupHasCountryLabel = true;
-        break;
-      }
-    }
-    if (groupHasCountryLabel) break;
-  }
-
-  // country label found: unselect all ROMs in group that do NOT have it
-  if (groupHasCountryLabel) {
-    for (const rom of roms) {
-      if (!rom.selected) continue;
-      let romHasCountryLabel = false;
-      for (const label of rom.labels) {
-        if (label.includes(country)) {
-          romHasCountryLabel = true;
-          break;
-        }
-      }
-      if (!romHasCountryLabel) rom.selected = false;
-    }
-  }
-
-  // report back if country label was found in ROM group
-  return groupHasCountryLabel;
+  const nonCountryRoms = roms.filter((rom) => !rom.labels.some((label) => label.includes(country)));
+  nonCountryRoms.forEach((rom) => rom.selected = false);
+  return true;
 };
 
 export default unselectByCountry;
