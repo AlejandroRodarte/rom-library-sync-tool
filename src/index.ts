06864e0933d003abe3e1ed398bd8d6c92e69172e
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-12-21 17:04:39 -0600

    add: unselect country ROMs with too many labels
    Keep selected the country ROM with the least amount of labels

--- src/index.ts
+++ src/index.ts
@@ -447,6 +447,33 @@ const main = async () => {
           versionLabelFound,
         );
       }
+
+      const countryRomsSelected = roms.filter((rom) => {
+        const hasCountryLabel = rom.labels.some((label) =>
+          label.includes(countryLabel),
+        );
+        const isSelected = rom.selected;
+        return hasCountryLabel && isSelected;
+      });
+
+      if (countryRomsSelected.length > 1) {
+        let minLabelAmount = -1;
+        const firstRom = countryRomsSelected[0];
+        if (firstRom) minLabelAmount = firstRom.labels.length;
+
+        for (let index = 1; index < countryRomsSelected.length; index++) {
+          const rom = countryRomsSelected[index];
+          if (rom) {
+            const labelAmount = rom.labels.length;
+            if (labelAmount < minLabelAmount) minLabelAmount = labelAmount;
+          }
+        }
+
+        countryRomsSelected.forEach((rom) => {
+          if (rom.labels.length > minLabelAmount) rom.selected = false;
+        });
+      }
+
       const romsSelected = roms.reduce((acc, rom) => {
         if (rom.selected) acc++;
         return acc;
