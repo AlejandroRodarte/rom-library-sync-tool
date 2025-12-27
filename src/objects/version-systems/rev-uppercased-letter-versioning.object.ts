ab42a4f13f31be1a74d87950a10067c368be43a5
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-12-26 20:33:59 -0600

    fix: apply literal dash on REV-[a-zA-Z] versioning system

--- src/objects/version-systems/rev-uppercased-letter-versioning.object.ts
+++ src/objects/version-systems/rev-uppercased-letter-versioning.object.ts
@@ -1,10 +1,10 @@
 import type { VersionSystem } from "../../types.js";
 
 const revUppercasedLetterVersioning: VersionSystem = {
-  pattern: /^REV-[a-zA-Z]$/,
+  pattern: /^REV\-[a-zA-Z]$/,
   compareFn: (label1, label2) => {
-    const num1 = label1.replace(/REV-/, "").charCodeAt(0);
-    const num2 = label2.replace(/REV-/, "").charCodeAt(0);
+    const num1 = label1.replace(/REV\-/, "").charCodeAt(0);
+    const num2 = label2.replace(/REV\-/, "").charCodeAt(0);
     if (num1 > num2) return 1;
     else if (num1 < num2) return -1;
     else return 0;
