b4b3cfdc1b58739335af6424eea797406c01282a
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2025-12-27 20:14:19 -0600

    update: ROMs dir path to where my actual PC ROMs live

--- src/constants/paths.constants.ts
+++ src/constants/paths.constants.ts
@@ -1,4 +1,5 @@
 import path from "node:path";
 
-export const ROMS_DIR_PATH = "/home/alejandro/Downloads/myrient";
+export const ROMS_DIR_PATH =
+  "/media/alejandro/3tb-ssd/home/alejandro/games/roms";
 export const DATA_DIR_PATH = path.resolve(process.env.PWD || __dirname, "data");
