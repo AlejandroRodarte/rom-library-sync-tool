dfe37f2cb60b0f7664b19ab7c0d5d82cc7f2e0fd
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-01 15:01:53 -0600

    refactor: let unselectByWhitelistedLabelsBasePriorityList call unselectByWhitelistedLabels

--- src/helpers/unselect-by-whitelisted-labels-base-priority-list.helper.ts
+++ src/helpers/unselect-by-whitelisted-labels-base-priority-list.helper.ts
@@ -1,28 +1,8 @@
 import WHITELISTED_LABELS_BASE_PRIORITY_LIST from "../constants/whitelisted-labels-base-priority-list.constant.js";
 import type { Rom } from "../types.js";
+import unselectByWhitelistedLabels from "./unselect-by-whitelisted-labels.helper.js";
 
-const unselectByWhitelistedLabelsBasePriorityList = (roms: Rom[]): void => {
-  let selectedRoms = roms.filter((rom) => rom.selected);
-
-  let romAmount = selectedRoms.length;
-  if (romAmount === 1) return;
-
-  for (const wantedExactLabel of WHITELISTED_LABELS_BASE_PRIORITY_LIST) {
-    const romsWithoutWantedLabel = selectedRoms.filter(
-      (rom) => !rom.labels.includes(wantedExactLabel),
-    );
-
-    const romSetLacksWantedLabel =
-      romsWithoutWantedLabel.length === selectedRoms.length;
-    if (romSetLacksWantedLabel) continue;
-
-    for (const romToUnselect of romsWithoutWantedLabel) {
-      romToUnselect.selected = false;
-      romAmount--;
-      if (romAmount === 1) return;
-      selectedRoms = roms.filter((rom) => rom.selected);
-    }
-  }
-};
+const unselectByWhitelistedLabelsBasePriorityList = (roms: Rom[]): void =>
+  unselectByWhitelistedLabels(roms, WHITELISTED_LABELS_BASE_PRIORITY_LIST);
 
 export default unselectByWhitelistedLabelsBasePriorityList;
