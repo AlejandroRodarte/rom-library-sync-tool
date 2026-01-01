2240d1fcb962705d46e1673348c83e1f2cc14ae2
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-01 16:18:59 -0600

    add: create "fileIO" object that compacts all helpers/file-io functions

---
+++ src/helpers/file-io/index.ts
@@ -0,0 +1,11 @@
+import writeConsoleDiffFile from "./write-console-diff-file.helper.js";
+import writeConsoleFiles from "./write-console-files.helper.js";
+import writeRomFilenamesToConsoleFile from "./write-rom-filenames-to-console-file.helper.js";
+
+const fileIO = {
+  writeConsoleFiles,
+  writeConsoleDiffFile,
+  writeRomFilenamesToConsoleFile,
+};
+
+export default fileIO;
