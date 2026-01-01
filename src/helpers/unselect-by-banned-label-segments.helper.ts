82eaed3c192509580a5141879c3a0650e4bba936
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-01 12:11:46 -0600

    add: new unselectByBannedLabelSegments helper function

---
+++ src/helpers/unselect-by-banned-label-segments.helper.ts
@@ -0,0 +1,27 @@
+import type { Rom } from "../types.js";
+
+const unselectByBannedLabelSegments = (
+  roms: Rom[],
+  labelSegments: string[],
+  keepSelected = 1,
+): void => {
+  let selectedRoms = roms.filter((rom) => rom.selected);
+
+  let selectedRomAmount = selectedRoms.length;
+  if (selectedRomAmount === keepSelected) return;
+
+  for (const bannedLabelSegment of labelSegments) {
+    const romsWithBannedLabelSegment = selectedRoms.filter((rom) =>
+      rom.labels.some((label) => label.includes(bannedLabelSegment)),
+    );
+
+    for (const romToUnselect of romsWithBannedLabelSegment) {
+      romToUnselect.selected = false;
+      selectedRomAmount--;
+      if (selectedRomAmount === keepSelected) return;
+      selectedRoms = roms.filter((rom) => rom.selected);
+    }
+  }
+};
+
+export default unselectByBannedLabelSegments;
