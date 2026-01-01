ed11461b58c21172a9c3001d27e0621d85a325f9
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-01 16:02:26 -0600

    add: new functions to the main "build" object

--- src/helpers/unselect/index.ts
+++ src/helpers/unselect/index.ts
@@ -1,8 +1,10 @@
 import byBannedLabelSegments from "./by-banned-label-segments.helper.js";
 import byBannedLabelsBasePriorityList from "./by-banned-labels-base-priority-list.helper.js";
 import byBannedLabels from "./by-banned-labels.helper.js";
+import byCountryBasePriorityList from "./by-country-base-priority-list.helper.js";
 import byCountry from "./by-country.helper.js";
 import byLanguageAmount from "./by-language-amount.helper.js";
+import byLanguagesBasePriorityList from "./by-languages-base-priority-list.helper.js";
 import byLanguages from "./by-languages.helper.js";
 import byPALAndNTSCLabels from "./by-pal-and-ntsc-labels.helper.js";
 import bySpecialFlags from "./by-special-flags.helper.js";
@@ -13,7 +15,9 @@ import byWhitelistedLabels from "./by-whitelisted-labels.helper.js";
 
 const unselect = {
   byCountry,
+  byCountryBasePriorityList,
   byLanguages,
+  byLanguagesBasePriorityList,
   byLanguageAmount,
   byBannedLabels,
   byBannedLabelSegments,
