89194199bb0762ddd1edb1e2448b71c5d9e387b0
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-01 15:26:18 -0600

    refactor: condense all unselect functions under a single object

---
+++ src/helpers/unselect/index.ts
@@ -0,0 +1,29 @@
+import byBannedLabelSegments from "./by-banned-label-segments.helper.js";
+import byBannedLabelsBasePriorityList from "./by-banned-labels-base-priority-list.helper.js";
+import byBannedLabels from "./by-banned-labels.helper.js";
+import byCountry from "./by-country.helper.js";
+import byLanguageAmount from "./by-language-amount.helper.js";
+import byLanguages from "./by-languages.helper.js";
+import byPALAndNTSCLabels from "./by-pal-and-ntsc-labels.helper.js";
+import bySpecialFlags from "./by-special-flags.helper.js";
+import byVersionSystem from "./by-version-systems.helper.js";
+import byVersionsPriorityList from "./by-versions-priority-list.helper.js";
+import byWhitelistedLabelsBasePriorityList from "./by-whitelisted-labels-base-priority-list.helper.js";
+import byWhitelistedLabels from "./by-whitelisted-labels.helper.js";
+
+const unselect = {
+  byCountry,
+  byLanguages,
+  byLanguageAmount,
+  byBannedLabels,
+  byBannedLabelSegments,
+  byWhitelistedLabels,
+  byVersionSystem,
+  bySpecialFlags,
+  byPALAndNTSCLabels,
+  byVersionsPriorityList,
+  byBannedLabelsBasePriorityList,
+  byWhitelistedLabelsBasePriorityList,
+};
+
+export default unselect;
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
 import buildEmptyConsolesObject from "./helpers/build-empty-consoles-object.helper.js";
-import byCountry from "./helpers/unselect/by-country.helper.js";
 import COUNTRY_PRIORITY_LIST from "./constants/country-priority-list.constant.js";
-import byVersionsPriorityList from "./helpers/unselect/by-versions-priority-list.helper.js";
 import addRomsToConsole from "./helpers/add-roms-to-console.helper.js";
-import byLanguages from "./helpers/unselect/by-languages.helper.js";
 import LANGUAGE_PRIORITY_LIST from "./constants/language-priority-list.constant.js";
-import byPALAndNTSCLabels from "./helpers/unselect/by-pal-and-ntsc-labels.helper.js";
-import bySpecialFlags from "./helpers/unselect/by-special-flags.helper.js";
-import byBannedLabelsBasePriorityList from "./helpers/unselect/by-banned-labels-base-priority-list.helper.js";
-import byLanguageAmount from "./helpers/unselect/by-language-amount.helper.js";
 import { BIOS_TITLE_SEGMENT } from "./constants/title-segments.constnats.js";
 import printConsoleDuplicates from "./helpers/print-console-duplicates.helper.js";
 import printFinalConsolesReport from "./helpers/print-final-consoles-report.helper.js";
 import writeConsoleFiles from "./helpers/write-console-files.helper.js";
 import getGroupsFromConsoleRomsDir from "./helpers/get-groups-from-console-roms-dir.helper.js";
-import byBannedLabelSegments from "./helpers/unselect/by-banned-label-segments.helper.js";
-import byWhitelistedLabelsBasePriorityList from "./helpers/unselect/by-whitelisted-labels-base-priority-list.helper.js";
+import unselect from "./helpers/unselect/index.js";
 
 const main = async () => {
   const consoles = buildEmptyConsolesObject();
@@ -28,17 +20,17 @@ const main = async () => {
       const keepSelected = 1;
 
       if (!titleIsBios) {
-        byCountry(roms, COUNTRY_PRIORITY_LIST);
-        byLanguages(roms, LANGUAGE_PRIORITY_LIST);
-        byLanguageAmount(roms);
+        unselect.byCountry(roms, COUNTRY_PRIORITY_LIST);
+        unselect.byLanguages(roms, LANGUAGE_PRIORITY_LIST);
+        unselect.byLanguageAmount(roms);
       }
-      bySpecialFlags(roms);
-      byBannedLabelSegments(roms, ["Disk"]);
-      byVersionsPriorityList(roms);
+      unselect.bySpecialFlags(roms);
+      unselect.byBannedLabelSegments(roms, ["Disk"]);
+      unselect.byVersionsPriorityList(roms);
       if (!titleIsBios) {
-        byPALAndNTSCLabels(roms);
-        byBannedLabelsBasePriorityList(roms);
-        byWhitelistedLabelsBasePriorityList(roms);
+        unselect.byPALAndNTSCLabels(roms);
+        unselect.byBannedLabelsBasePriorityList(roms);
+        unselect.byWhitelistedLabelsBasePriorityList(roms);
       }
 
       addRomsToConsole(roms, konsole, title);
