492150ee4b8389a153c328e77d89196758e6ddc1
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-01 15:49:01 -0600

    add: new build object that compacts all helper build functions

---
+++ src/helpers/build/index.ts
@@ -0,0 +1,17 @@
+import emptyConsoles from "./empty-consoles.helper.js";
+import groupsFromConsoleName from "./groups-from-console-name.helper.js";
+import groupsFromFilenames from "./groups-from-filenames.helper.js";
+import labelsAndLanguagesFromFilename from "./labels-and-languages-from-filename.helper.js";
+import selectedRomFilenamesFromConsole from "./selected-rom-filenames-from-console.helper.js";
+import specialFlagsFromRoms from "./special-flags-from-roms.helper.js";
+
+const build = {
+  emptyConsoles,
+  groupsFromConsoleName,
+  groupsFromFilenames,
+  labelsAndLanguagesFromFilename,
+  selectedRomFilenamesFromConsole,
+  specialFlagsFromRoms,
+};
+
+export default build;
