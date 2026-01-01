c453895e7eb498af805645f62553e2531c813b93
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-01 16:12:05 -0600

    add: create "log" object that compacts all helpers/log functions

---
+++ src/helpers/log/index.ts
@@ -0,0 +1,9 @@
+import consoleDuplicates from "./console-duplicates.helper.js";
+import consolesReport from "./consoles-report.helper.js";
+
+const log = {
+  consoleDuplicates,
+  consolesReport,
+};
+
+export default log;
