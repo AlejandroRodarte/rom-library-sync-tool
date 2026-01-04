b8386c1edcdba1e399fc490d0674fe70c08c01bb
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-04 17:34:27 -0600

    add: new fileIO.writeDeleteFileLineToDiffFile helper function

---
+++ src/helpers/file-io/write-delete-file-line-to-diff-file.helper.ts
@@ -0,0 +1,19 @@
+import type { PathLike } from "node:fs";
+import writeToFileOrDelete from "./write-to-file-or-delete.helper.js";
+import type { FileHandle } from "node:fs/promises";
+
+const writeDeleteFileLineToDiffFile = async (
+  filename: string,
+  diffFilePath: PathLike,
+  diffFileHandle: FileHandle,
+): Promise<Error | undefined> => {
+  const diffFileWriteError = await writeToFileOrDelete(
+    diffFilePath,
+    diffFileHandle,
+    `remove-file|"${filename}"\n`,
+    "utf8",
+  );
+  if (diffFileWriteError) return diffFileWriteError;
+};
+
+export default writeDeleteFileLineToDiffFile;
