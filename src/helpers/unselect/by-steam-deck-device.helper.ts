4e9d9e0e39f0dd77897c53e9831b95ab16764285
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-04 22:51:11 -0600

    add: new unselect.bySteamDeckDevice helper function

---
+++ src/helpers/unselect/by-steam-deck-device.helper.ts
@@ -0,0 +1,23 @@
+import type Title from "../../classes/title.class.js";
+import { BIOS_TITLE_SEGMENT } from "../../constants/title-segments.constnats.js";
+import byBiosTitle from "./by-bios-title.helper.js";
+import byNormalTitle from "./by-normal-title.helper.js";
+
+const bySteamDeckDevice = (name: string, title: Title): void => {
+  let type: "normal" | "bios" = "normal";
+  const titleIsBios = name.includes(BIOS_TITLE_SEGMENT);
+  if (titleIsBios) type = "bios";
+
+  switch (type) {
+    case "normal":
+      byNormalTitle(title);
+      break;
+    case "bios":
+      byBiosTitle(title);
+      break;
+    default:
+      break;
+  }
+};
+
+export default bySteamDeckDevice;
