2e45775ed8be018e48491f416bb33d459d21ba1c
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-05 17:40:00 -0600

    add: new sftp functons into our main global sftp object

--- src/helpers/sftp/index.ts
+++ src/helpers/sftp/index.ts
@@ -1,7 +1,9 @@
+import addFile from "./add-file.helper.js";
 import connect from "./connect.helper.js";
+import deleteFile from "./delete-file.helper.js";
 import disconnect from "./disconnect.helper.js";
 import exists from "./exists.helper.js";
 
-const sftp = { connect, disconnect, exists };
+const sftp = { connect, disconnect, exists, addFile, deleteFile };
 
 export default sftp;
