51fa97e6e612d79ce5a1f1dfcb19b6aebe28026a
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-06 15:47:43 -0600

    chore: formatting

--- src/helpers/file-io/read-utf8-file.helper.ts
+++ src/helpers/file-io/read-utf8-file.helper.ts
@@ -1,14 +1,19 @@
 import fs from "node:fs/promises";
 import type { PathLike } from "node:fs";
 
-const readUtf8FileIntoString = async (filePath: PathLike): Promise<[string, undefined] | [undefined, Error]> => {
+const readUtf8FileIntoString = async (
+  filePath: PathLike,
+): Promise<[string, undefined] | [undefined, Error]> => {
   try {
     const buffer = await fs.readFile(filePath, { encoding: "utf8", flag: "r" });
     return [buffer, undefined];
   } catch (e: unknown) {
     if (e instanceof Error) return [undefined, e];
     else
-      return [undefined, new Error("readFileIntoBuffer(): an unknown error has occured.")];
+      return [
+        undefined,
+        new Error("readFileIntoBuffer(): an unknown error has occured."),
+      ];
   }
 };
 
