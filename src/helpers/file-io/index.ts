f83ea1b6d788a3b224229fe1e0a188aa8ae9a551
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-06 10:14:01 -0600

    add: new fileIO functions to global object

--- src/helpers/file-io/index.ts
+++ src/helpers/file-io/index.ts
@@ -18,6 +18,10 @@ import deleteAllConsoleDiffFiles from "./delete-all-console-diff-files.helper.js
 import dirExists from "./dir-exists.helper.js";
 import checkDeviceDirPaths from "./check-device-dir-paths.helper.js";
 import dirExistsAndIsReadable from "./dir-exists-and-is-readable.helper.js";
+import fileExists from "./file-exists.helper.js";
+import fileIsEmpty from "./file-is-empty.helper.js";
+import findDeleteAndOpenWriteOnlyFile from "./find-delete-and-open-write-only-file.helper.js";
+import fileExistsAndReadUtf8Lines from "./file-exists-and-read-utf8-lines.helper.js";
 
 const fileIO = {
   writeRomFilenamesToConsoleFile,
@@ -40,6 +44,10 @@ const fileIO = {
   dirExists,
   checkDeviceDirPaths,
   dirExistsAndIsReadable,
+  fileExists,
+  fileIsEmpty,
+  findDeleteAndOpenWriteOnlyFile,
+  fileExistsAndReadUtf8Lines,
 };
 
 export default fileIO;
