af8c09a4a30be1a9c87bfc965bfa0ef35521bd48
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-06 10:21:39 -0600

    add: new devices global object

---
+++ src/helpers/devices/index.ts
@@ -0,0 +1,9 @@
+import updateLocal from "./update-local.helper.js";
+import updateSteamDeck from "./update-steam-deck.helper.js";
+
+const devices = {
+  updateSteamDeck,
+  updateLocal,
+};
+
+export default devices;
