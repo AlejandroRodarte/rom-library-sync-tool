0f2d8f8a6496fac5132c533b738843d32aa0cb78
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-06 10:19:56 -0600

    add: new build.diffLineFromDiffAction helper function

---
+++ src/helpers/build/diff-line-from-diff-action.helper.ts
@@ -0,0 +1,13 @@
+import type { DiffAction } from "../../types.js";
+
+const diffLineFromDiffAction = (diffAction: DiffAction): string {
+  switch (diffAction.type) {
+    case "add-file": return `add-file|${diffAction.data.filename}`;
+    case "remove-file": return `remove-file|${diffAction.data.filename}`;
+
+    default:
+      throw new Error("Unrecognized diff action. This should be unreachable.");
+  }
+};
+
+export default diffLineFromDiffAction;
