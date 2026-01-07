bc282718fc16c0fad62ef0bec59995764b1bd273
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-06 22:26:53 -0600

    fix: bad arrow function syntax

--- src/helpers/build/diff-line-from-diff-action.helper.ts
+++ src/helpers/build/diff-line-from-diff-action.helper.ts
@@ -1,9 +1,11 @@
 import type { DiffAction } from "../../types.js";
 
-const diffLineFromDiffAction = (diffAction: DiffAction): string {
+const diffLineFromDiffAction = (diffAction: DiffAction): string => {
   switch (diffAction.type) {
-    case "add-file": return `add-file|${diffAction.data.filename}`;
-    case "remove-file": return `remove-file|${diffAction.data.filename}`;
+    case "add-file":
+      return `add-file|${diffAction.data.filename}`;
+    case "remove-file":
+      return `remove-file|${diffAction.data.filename}`;
 
     default:
       throw new Error("Unrecognized diff action. This should be unreachable.");
