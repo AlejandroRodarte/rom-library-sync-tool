c5d2fe6f6819ba069d7b2e4cc459a3a2c6a56aae
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-06 22:21:27 -0600

    add: new typeGuards global object to condense all of our type guard functions

---
+++ src/helpers/typescript/guards/index.ts
@@ -0,0 +1,9 @@
+import isErrnoException from "./is-errno-exception.helper.js";
+import isSftpError from "./is-sftp-error.helper.js";
+
+const typeGuards = {
+  isErrnoException,
+  isSftpError,
+};
+
+export default typeGuards;
