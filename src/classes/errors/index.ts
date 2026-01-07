cc092f8851a77c340963d0e84105e6a57133be3d
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-06 22:17:04 -0600

    add: new custom errors to our global errorClasses object

--- src/classes/errors/index.ts
+++ src/classes/errors/index.ts
@@ -1,9 +1,29 @@
+import FsNotFoundError from "./fs-not-found-error.class.js";
 import SftpConnectionError from "./sftp-connection-error.class.js";
 import SftpDisconnectionError from "./sftp-disconnection-error.class.js";
+import FsUnauthorizedError from "./fs-unauthorized-error.class.js";
+import UnknownError from "./unknown-error.class.js";
+import FsWrongTypeError from "./fs-wrong-type-error.class.js";
+import SftpNotFoundError from "./sftp-not-found-error.class.js";
+import SftpWrongTypeError from "./sftp-wrong-type-error.class.js";
+import SftpBadPathError from "./sftp-bad-path.class.js";
+import SftpUnauthorizedError from "./sftp-unauthorized-error.class.js";
+import SftpBadCredentialsError from "./sftp-bad-credentials.class.js";
+import FsFileExistsError from "./fs-file-exists-error.class.js";
 
 const errorClasses = {
   SftpConnectionError,
   SftpDisconnectionError,
+  UnknownError,
+  FsUnauthorizedError,
+  FsNotFoundError,
+  FsFileExistsError,
+  FsWrongTypeError,
+  SftpNotFoundError,
+  SftpWrongTypeError,
+  SftpBadPathError,
+  SftpUnauthorizedError,
+  SftpBadCredentialsError,
 };
 
 export default errorClasses;
