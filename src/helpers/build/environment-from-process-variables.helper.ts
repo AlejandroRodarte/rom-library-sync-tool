653a1fa7f663bb32e03e88f8c828025445029689
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-07 20:05:59 -0600

    refactor: make build.environmentFromProcessVariables() return custom errors

--- src/helpers/build/environment-from-process-variables.helper.ts
+++ src/helpers/build/environment-from-process-variables.helper.ts
@@ -1,29 +1,33 @@
+import AppNotFoundError from "../../classes/errors/app-not-found-error.class.js";
+import AppValidationError from "../../classes/errors/app-validation-error.class.js";
 import type { Environment } from "../../types.js";
 
 const environmentFromProcessVariables = (): Environment => {
   const rawUpdateLocal = process.env.UPDATE_LOCAL;
   if (!rawUpdateLocal)
-    throw new Error(
+    throw new AppNotFoundError(
       "Please assign a value to UPDATE_LOCAL environment variable.",
     );
   const isRawUpdateLocalValid = /^[0-1]{1}$/.test(rawUpdateLocal);
   if (!isRawUpdateLocalValid)
-    throw new Error("UPDATE_LOCAL must be either a 0 or a 1.");
+    throw new AppValidationError("UPDATE_LOCAL must be either a 0 or a 1.");
   const updateLocal = +rawUpdateLocal === 1 ? true : false;
 
   const rawUpdateSteamDeck = process.env.UPDATE_STEAM_DECK;
   if (!rawUpdateSteamDeck)
-    throw new Error(
+    throw new AppNotFoundError(
       "Please assign a value to UPDATE_STEAM_DECK environment variable.",
     );
   const isRawUpdateSteamDeckValid = /^[0-1]{1}$/.test(rawUpdateSteamDeck);
   if (!isRawUpdateSteamDeckValid)
-    throw new Error("UPDATE_STEAM_DECK must be either a 0 or a 1.");
+    throw new AppValidationError(
+      "UPDATE_STEAM_DECK must be either a 0 or a 1.",
+    );
   const updateSteamDeck = +rawUpdateSteamDeck === 1 ? true : false;
 
   const steamDeckHost = process.env.STEAM_DECK_HOST;
   if (!steamDeckHost)
-    throw new Error(
+    throw new AppNotFoundError(
       "Please assign a value to STEAM_DECK_HOST environment variable.",
     );
   const isSteamDeckHostValid =
@@ -31,30 +35,34 @@ const environmentFromProcessVariables = (): Environment => {
       steamDeckHost,
     );
   if (!isSteamDeckHostValid)
-    throw new Error("STEAM_DECK_HOST must be a valid IPv4 address.");
+    throw new AppValidationError(
+      "STEAM_DECK_HOST must be a valid IPv4 address.",
+    );
 
   const rawSteamDeckPort = process.env.STEAM_DECK_PORT;
   if (!rawSteamDeckPort)
-    throw new Error(
+    throw new AppNotFoundError(
       "Please assign a value to STEAM_DECK_PORT environment variable.",
     );
   const isSteamDeckPortANumber = /^[0-9]+$/.test(rawSteamDeckPort);
   if (!isSteamDeckPortANumber)
-    throw new Error("STEAM_DECK_PORT must be a number.");
+    throw new AppValidationError("STEAM_DECK_PORT must be a number.");
   const steamDeckPort = +rawSteamDeckPort;
   const isSteamDeckPortInRange = steamDeckPort >= 1 && steamDeckPort <= 65535;
   if (!isSteamDeckPortInRange)
-    throw new Error("STEAM_DECK_PORT must be a valid port [0-65535].");
+    throw new AppValidationError(
+      "STEAM_DECK_PORT must be a valid port [0-65535].",
+    );
 
   const steamDeckUsername = process.env.STEAM_DECK_USERNAME;
   if (!steamDeckUsername)
-    throw new Error(
+    throw new AppNotFoundError(
       "Please assign a value to STEAM_DECK_USERNAME environment variable.",
     );
 
   const steamDeckPassword = process.env.STEAM_DECK_PASSWORD;
   if (!steamDeckPassword)
-    throw new Error(
+    throw new AppNotFoundError(
       "Please assign a value to STEAM_DECK_PASSWORD environment variable.",
     );
 
