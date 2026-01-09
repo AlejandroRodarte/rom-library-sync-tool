e71d74a97b6afbbd2e71ea59a1faa04a738d442d
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-09 17:55:19 -0600

    fix: faulty isErrnoException type guard

--- src/helpers/typescript/guards/is-errno-exception.helper.ts
+++ src/helpers/typescript/guards/is-errno-exception.helper.ts
@@ -1,8 +1,8 @@
 const isErrnoException = (error: unknown): error is NodeJS.ErrnoException => {
   if (error instanceof Error) {
     const castedError = error as NodeJS.ErrnoException;
-    const hasErrno = typeof castedError.errno === "string";
-    const hasCode = typeof castedError.code !== "number";
+    const hasErrno = typeof castedError.errno === "number";
+    const hasCode = typeof castedError.code === "string";
     return hasErrno && hasCode;
   }
 
