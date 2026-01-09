3640e6438c8fa57e44fa6cd705340ad33c60f339
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-09 14:50:46 -0600

    refactor: use path.resolve for our main DATA_DIR_PATH

--- src/constants/paths.constants.ts
+++ src/constants/paths.constants.ts
@@ -1,5 +1,5 @@
 import path from "node:path";
 
-const DATA_DIR_PATH = path.join(process.env.PWD || __dirname, "data");
+const DATA_DIR_PATH = path.resolve(process.env.PWD || __dirname, "data");
 
 export const DEVICES_DIR_PATH = path.join(DATA_DIR_PATH, "devices");
