3f447195262413649dfd7cfa82b5dc0e22fae4a4
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-10 17:57:18 -0600

    add: use app.validateDbPathsWithDevices in main function

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ import fileIO from "./helpers/file-io/index.js";
 import log from "./helpers/log/index.js";
 import Device from "./classes/device.class.js";
 import unselect from "./helpers/unselect/index.js";
-import path from "node:path";
-import build from "./helpers/build/index.js";
+import app from "./helpers/app/index.js";
 
 const main = async () => {
   const devices = ENVIRONMENT.options.filter.devices.map(
@@ -12,53 +11,10 @@ const main = async () => {
       new Device(deviceName, ENVIRONMENT.devices[deviceName].consoles),
   );
 
-  const readableDirPaths: string[] = [
-    ENVIRONMENT.paths.dbs.roms,
-    ENVIRONMENT.paths.dbs.media,
-    ENVIRONMENT.paths.dbs.gamelists,
-  ];
-  const readableAndWritableDirPaths: string[] = [];
-
-  for (const device of devices)
-    readableAndWritableDirPaths.push(
-      device.paths.base,
-      device.paths.diffs,
-      device.paths.lists,
-      device.paths.failed,
-    );
-
-  for (const consoleName of build.consoleNamesFromDevices(devices))
-    readableDirPaths.push(
-      path.join(ENVIRONMENT.paths.dbs.roms, consoleName),
-      path.join(ENVIRONMENT.paths.dbs.media, consoleName),
-      path.join(ENVIRONMENT.paths.dbs.gamelists, consoleName),
-    );
-
-  const [areAllDirPathsReadable, readableDirPathsError] =
-    await fileIO.allDirsExistAndAreReadable(readableDirPaths);
-  if (readableDirPathsError) {
-    console.log(`${readableDirPathsError.reason}. Terminating.`);
+  const validateDbPathsError = await app.validateDbPathsWithDevices(devices);
+  if (validateDbPathsError) {
+    console.log(`${validateDbPathsError.reason}. Terminating.`);
     return;
-  }
-  if (!areAllDirPathsReadable) {
-    console.log(
-      `Not all of the following directories exist and are readable:\n${readableDirPaths.join("\n")}\nMake sure all of them exist and are readable. Terminating.`,
-    );
-    return;
-  }
-
-  const [areAllDirPathsReadableAndWritable, readableAndWritableDirPathsError] =
-    await fileIO.allDirsExistAndAreReadableAndWritable(
-      readableAndWritableDirPaths,
-    );
-  if (readableAndWritableDirPathsError) {
-    console.log(`${readableAndWritableDirPathsError.reason}. Terminating.`);
-    return;
-  }
-  if (!areAllDirPathsReadableAndWritable) {
-    console.log(
-      `Not all of the following directories exist and are readable and writable:\n${readableDirPaths.join("\n")}\nMake sure all of them exist and are readable and writable. Terminating.`,
-    );
   }
 
   for (const device of devices) {
