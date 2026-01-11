11cc2861c42f7eca82ce4901ef7a6f52b24849df
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-10 20:15:42 -0600

    add: toString() method to all custom errors

--- src/classes/errors/unknown-error.class.ts
+++ src/classes/errors/unknown-error.class.ts
@@ -20,6 +20,21 @@ class UnknownError extends CustomError {
   public addReason(reason: string): void {
     this._reasons.push(reason);
   }
+
+  public toString(): string {
+    let content = "";
+
+    content += `!!!!! Error !!!!!\n`;
+    content += `Type: ${this.type}\n`;
+    content += `Status code: ${this.code}\n`;
+    content += `Code: ${this.code}.\n`;
+    content += `Message: ${this.message}\n`;
+
+    content += `Reasons:\n`;
+    for (const reason of this._reasons) content += `${reason}\n`;
+
+    return content;
+  }
 }
 
 export default UnknownError;
