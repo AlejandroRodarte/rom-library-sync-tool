41667340cab2e7bfc62334d99d8a882c9b99c7f3
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-13 21:33:44 -0600

    delete: un-needed build helper functions

--- src/helpers/build/index.ts
+++ src/helpers/build/index.ts
@@ -1,4 +1,3 @@
-import emptyConsoles from "./empty-consoles.helper.js";
 import titlesFromRomsDirPath from "./titles-from-roms-dir-path.helper.js";
 import titlesFromFilenames from "./titles-from-filenames.helper.js";
 import labelsAndLanguagesFromFilename from "./labels-and-languages-from-filename.helper.js";
@@ -8,7 +7,6 @@ import filenameIndexesToAddAndDelete from "./filename-indexes-to-add-and-delete.
 import diffActionFromDiffLine from "./diff-action-from-diff-line.helper.js";
 import diffLineFromDiffAction from "./diff-line-from-diff-action.helper.js";
 import steamDeckSftpClient from "./steam-deck-sftp-client.helper.js";
-import deviceNamesFromFilterDevicesList from "./device-names-from-filter-devices-list.helper.js";
 import deviceNamesFromSyncDevicesList from "./device-names-from-sync-devices-list.helper.js";
 import deviceNamesFromDevicesList from "./device-names-from-devices-list.helper.js";
 import consoleNamesFromConsolesList from "./console-names-from-consoles-list.helper.js";
@@ -16,7 +14,6 @@ import mediaNamesFromMediasList from "./media-names-from-medias-list.helper.js";
 import intersectStringArraySimple from "./intersect-string-array-simple.helper.js";
 
 const build = {
-  emptyConsoles,
   titlesFromRomsDirPath,
   titlesFromFilenames,
   labelsAndLanguagesFromFilename,
@@ -26,7 +23,6 @@ const build = {
   diffActionFromDiffLine,
   diffLineFromDiffAction,
   steamDeckSftpClient,
-  deviceNamesFromFilterDevicesList,
   deviceNamesFromSyncDevicesList,
   deviceNamesFromDevicesList,
   consoleNamesFromConsolesList,
