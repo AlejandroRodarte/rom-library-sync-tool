5c9ce7f190c3ff67c904088c74344a6aeff40417
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-16 21:35:00 -0600

    add: new "specific code" field for DeviceFileIOLsError
    Specific codes describe reasons why a specific interface required method
    would fail. In this case, why DeviceFileIO.ls() would fail.

--- src/classes/errors/device-file-io-ls-error.class.ts
+++ src/classes/errors/device-file-io-ls-error.class.ts
@@ -1,6 +1,23 @@
 import CustomError from "./custom-error.abstract.class.js";
 import UniversalError from "./universal-error.class.js";
 
+// why would DeviceFileIO.ls() fail?
+// 1. because a component in the path is not valid
+// 2. because the path is valid, but does not exist
+// 3. because the path is valid and exists, but is not a directory
+// 4. because we are unauthorized to access it
+// 5. becase there was a connecton issue
+export const SPECIFIC_ERROR_CODES = [
+  "BAD_PATH_ERROR",
+  "PATH_DOES_NOT_EXIST_ERROR",
+  "NOT_A_DIRECTORY_ERROR",
+  "UNAUTHORIZED_ERROR",
+  "CONNECTION_ERROR",
+  "UNKNOWN_ERROR",
+] as const;
+
+export type SpecificErrorCode = (typeof SPECIFIC_ERROR_CODES)[number];
+
 class DeviceFileIOLsError extends CustomError {
   readonly type = "DeviceFileIOLsError";
   readonly status = 400;
@@ -11,9 +28,16 @@ class DeviceFileIOLsError extends CustomError {
   private _reason: string;
   private _linkedError: UniversalError | undefined;
 
-  constructor(reason: string, linkedError?: UniversalError) {
+  private _specificCode: SpecificErrorCode;
+
+  constructor(
+    reason: string,
+    specificCode: SpecificErrorCode,
+    linkedError?: UniversalError,
+  ) {
     super();
     this._reason = reason;
+    this._specificCode = specificCode;
     if (linkedError) this._linkedError = linkedError;
   }
 
