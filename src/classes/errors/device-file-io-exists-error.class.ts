bf54120c16b1ef39ead0b4da4b52eac718aa24e1
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-17 16:08:02 -0600

    add: specific error codes for DeviceFileIOExistsError

--- src/classes/errors/device-file-io-exists-error.class.ts
+++ src/classes/errors/device-file-io-exists-error.class.ts
@@ -1,6 +1,25 @@
 import CustomError from "./custom-error.abstract.class.js";
 import UniversalError from "./universal-error.class.js";
 
+// why would DeviceFileIO.exists() fail?
+// 1. because the path is not valid
+// 2. because the path is valid, but does not exist
+// 3. because the path exists, but is not of the correct type (file|dir|link)
+// 4. because we are unauthorized to access the path
+// 5. because there was a problem parsing the `rights` parameter
+// 6. because there was a connection issue
+export const SPECIFIC_ERROR_CODES = [
+  "BAD_PATH_ERROR",
+  "PATH_DOES_NOT_EXIST_ERROR",
+  "BAD_FILE_TYPE_ERROR",
+  "UNAUTHORIZED_ERROR",
+  "RIGHTS_VALIDATION_ERROR",
+  "CONNECTION_ERROR",
+  "UNKNOWN_ERROR",
+];
+
+export type SpecificErrorCode = (typeof SPECIFIC_ERROR_CODES)[number];
+
 class DeviceFileIOExistsError extends CustomError {
   readonly type = "DeviceFileIOExistsError";
   readonly status = 401;
@@ -11,14 +30,25 @@ class DeviceFileIOExistsError extends CustomError {
   private _reason: string;
   private _linkedError: UniversalError | undefined;
 
-  constructor(reason: string, linkedError?: UniversalError) {
+  private _specificCode: SpecificErrorCode;
+
+  constructor(
+    reason: string,
+    specificCode: SpecificErrorCode,
+    linkedError?: UniversalError,
+  ) {
     super();
     this._reason = reason;
+    this._specificCode = specificCode;
     if (linkedError) this._linkedError = linkedError;
   }
 
   get reason(): string {
     return this._reason;
+  }
+
+  get specificCode(): SpecificErrorCode {
+    return this._specificCode;
   }
 
   public toUniversalError(): UniversalError {
