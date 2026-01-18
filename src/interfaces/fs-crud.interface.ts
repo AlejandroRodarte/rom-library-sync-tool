9d6fdb0da53fbc67c8fbd17e2fd3d774c90af5e3
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-17 21:21:58 -0600

    add: new FsCrud interface

---
+++ src/interfaces/fs-crud.interface.ts
@@ -0,0 +1,40 @@
+import type AppValidationError from "../classes/errors/app-validation-error.class.js";
+import type FileIOBadCredentialsError from "../classes/errors/file-io-bad-credentials-error.class.js";
+import type FileIOBadPathError from "../classes/errors/file-io-bad-path-error.class.js";
+import type FileIOBadTypeError from "../classes/errors/file-io-bad-type-error.class.js";
+import type FileIOCircularReferenceError from "../classes/errors/file-io-circular-reference-error.class.js";
+import type FileIOConnectionError from "../classes/errors/file-io-connection-error.class.js";
+import type FileIOExistsError from "../classes/errors/file-io-exists-error.class.js";
+import type FileIONotFoundError from "../classes/errors/file-io-not-found-error.class.js";
+import type FileIOUnauthorizedError from "../classes/errors/file-io-unauthorized-error.class.js";
+import type UnknownError from "../classes/errors/unknown-error.class.js";
+
+type CommonErrors =
+  | UnknownError
+  | FileIONotFoundError
+  | FileIOBadTypeError
+  | FileIOBadPathError
+  | FileIOUnauthorizedError
+  | FileIOConnectionError
+  | FileIOBadCredentialsError
+  | AppValidationError;
+
+export type AddMethodError =
+  | CommonErrors
+  | FileIOExistsError
+  | FileIOCircularReferenceError;
+
+export type DeleteMethodError = CommonErrors;
+
+export interface FsCrud {
+  add: (
+    type: "file" | "dir",
+    srcPath: string,
+    dstPath: string,
+  ) => Promise<AddMethodError | undefined>;
+
+  delete: (
+    type: "file" | "dir",
+    path: string,
+  ) => Promise<DeleteMethodError | undefined>;
+}
