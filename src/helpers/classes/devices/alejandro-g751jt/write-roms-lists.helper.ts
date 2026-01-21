f887989fe9a59781c6703e4f63ea84ed6e764319
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-20 19:55:03 -0600

    add: re-add check because i am an apprehensive piece of shit

--- src/helpers/classes/devices/alejandro-g751jt/write-roms-lists.helper.ts
+++ src/helpers/classes/devices/alejandro-g751jt/write-roms-lists.helper.ts
@@ -1,5 +1,6 @@
 import type FileIOExtras from "../../../../classes/file-io/file-io-extras.class.js";
 import type { AlejandroG751JTPaths } from "../../../../interfaces/devices/alejandro-g751jt/alejandro-g751jt-paths.interface.js";
+import logger from "../../../../objects/logger.object.js";
 import type { ConsoleName } from "../../../../types/console-name.type.js";
 import type { DeviceName } from "../../../../types/device-name.type.js";
 import validateRomsListsDirs from "./validate-roms-lists-dirs.helper.js";
@@ -14,10 +15,26 @@ const writeRomsLists = async (
   await validateRomsListsDirs(name, paths, consoleNames, fileIOExtras);
 
   for (const consoleName of consoleNames) {
+    const deviceDir = paths.dirs["content-targets"].roms.consoles[consoleName];
+    if (!deviceDir) {
+      logger.fatal(
+        `Device dirpath for console ${consoleName} not found. Skipping.`,
+      );
+      return;
+    }
+
+    const projectFile = paths.files.project.lists.roms.consoles[consoleName];
+    if (!projectFile) {
+      logger.fatal(
+        `Project filepath for console ${consoleName} not found. Skipping.`,
+      );
+      return;
+    }
+
     await writeConsoleRomsList(
       {
-        deviceDir: paths.dirs["content-targets"].roms.consoles[consoleName]!,
-        projectFile: paths.files.project.lists.roms.consoles[consoleName]!,
+        deviceDir,
+        projectFile,
       },
       fileIOExtras.fileIO.ls,
     );
