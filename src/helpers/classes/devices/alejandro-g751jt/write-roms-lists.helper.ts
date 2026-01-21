ec7f5d9abc926442abb18b767cb15aaa1f0c64c7
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-20 19:38:45 -0600

    refactor: dumb down writeConsoleRomsList

--- src/helpers/classes/devices/alejandro-g751jt/write-roms-lists.helper.ts
+++ src/helpers/classes/devices/alejandro-g751jt/write-roms-lists.helper.ts
@@ -1,5 +1,6 @@
 import type FileIOExtras from "../../../../classes/file-io/file-io-extras.class.js";
 import type { AlejandroG751JTPaths } from "../../../../interfaces/devices/alejandro-g751jt/alejandro-g751jt-paths.interface.js";
+import logger from "../../../../objects/logger.object.js";
 import type { ConsoleName } from "../../../../types/console-name.type.js";
 import type { DeviceName } from "../../../../types/device-name.type.js";
 import validateRomsListsDirs from "./validate-roms-lists-dirs.helper.js";
@@ -14,7 +15,41 @@ const writeRomsLists = async (
   await validateRomsListsDirs(name, paths, consoleNames, fileIOExtras);
 
   for (const consoleName of consoleNames) {
-    await writeConsoleRomsList(name, consoleName, paths, fileIOExtras);
+    logger.trace(
+      `Beginning to write ROM list file for console ${consoleName}.`,
+    );
+
+    const deviceConsoleRomsDirPath =
+      paths.dirs["content-targets"].roms.consoles[consoleName];
+
+    if (!deviceConsoleRomsDirPath) {
+      logger.fatal(
+        `There is no ROM directory path for console ${consoleName} for device ${name}, even though we just checked it moments ago. Skipping this console.`,
+      );
+      return;
+    }
+
+    const deviceConsoleRomListFilePath =
+      paths.files.project.lists.roms.consoles[consoleName];
+
+    if (!deviceConsoleRomListFilePath) {
+      logger.fatal(
+        `(${name}) (${consoleName}): We failed to get ROM list filepath ${deviceConsoleRomListFilePath}, even though we just checked it moments ago. Skipping this console.`,
+      );
+      return;
+    }
+
+    logger.debug(
+      `(${name}): ROM directory path for console ${consoleName} = ${deviceConsoleRomsDirPath}`,
+    );
+
+    await writeConsoleRomsList(
+      {
+        device: deviceConsoleRomsDirPath,
+        project: deviceConsoleRomListFilePath,
+      },
+      fileIOExtras.fileIO.ls,
+    );
   }
 };
 
