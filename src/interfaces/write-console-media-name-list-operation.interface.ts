714ea0d5ac484a35103ecfc0de99f12cc0526cf5
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-22 16:43:01 -0600

    add: new WriteConsoleMediaNameListOperation interface

---
+++ src/interfaces/write-console-media-name-list-operation.interface.ts
@@ -0,0 +1,17 @@
+import type { ConsoleName } from "../types/console-name.type.js";
+import type { MediaName } from "../types/media-name.type.js";
+
+export interface WriteConsoleMediaNameListOperation {
+  paths: {
+    device: {
+      dir: string;
+    };
+    project: {
+      file: string;
+    };
+  };
+  names: {
+    console: ConsoleName;
+    media: MediaName;
+  };
+}
