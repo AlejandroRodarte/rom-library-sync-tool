4591e4ea41122eabc394666fe99865842c0d275b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-22 19:47:51 -0600

    remove: imports

--- src/helpers/classes/devices/alejandro-g751jt/write-media-lists.helper.ts
+++ src/helpers/classes/devices/alejandro-g751jt/write-media-lists.helper.ts
@@ -1,25 +1,12 @@
 import type FileIOExtras from "../../../../classes/file-io/file-io-extras.class.js";
-import { type DirAccessItem as FileIODirAccessItem } from "../../../../classes/file-io/file-io-extras.class.js";
 import type { AlejandroG751JTPaths } from "../../../../interfaces/devices/alejandro-g751jt/alejandro-g751jt-paths.interface.js";
-import type { WriteConsoleMediaNameListOperation } from "../../../../interfaces/write-console-media-name-list-operation.interface.js";
 import logger from "../../../../objects/logger.object.js";
 import type { ConsolesData } from "../../../../types/consoles-data.type.js";
 import type { MediaName } from "../../../../types/media-name.type.js";
-import allDirsExist, {
-  type DirAccessItem as FsDirAccessItem,
-} from "../../../extras/fs/all-dirs-exist.helper.js";
-import openFileForWriting from "../../../extras/fs/open-file-for-writing.helper.js";
-import writeLines from "../../../extras/fs/write-lines.helper.js";
 import buildMediaListsDirPaths from "./build-media-lists-dir-paths.helper.js";
 import buildWriteConsoleMediaNameListOperations from "./build-write-console-media-name-list-operations.helper.js";
 import validateMediaListsDirs from "./validate-media-lists-dirs.helper.js";
 import writeConsoleMediaNameLists from "./write-console-media-name-lists.helper.js";
-
-const fsExtras = {
-  allDirsExist,
-  openFileForWriting,
-  writeLines,
-};
 
 const writeMediaLists = async (
   paths: AlejandroG751JTPaths,
