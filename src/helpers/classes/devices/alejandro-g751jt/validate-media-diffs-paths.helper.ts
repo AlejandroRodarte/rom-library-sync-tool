66cc2a5e62dd2231c2a1fdfd7c1a7de7dd5bfd34
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-01-24 21:07:48 -0600

    delete: un-needed helper functions

--- src/helpers/classes/devices/alejandro-g751jt/validate-media-diffs-paths.helper.ts
+++
@@ -1,8 +0,0 @@
-const validateMediaDiffsPaths = async (paths: {
-  project: {
-    list: { dirs: string[]; files: string[] };
-    diff: { dirs: string[] };
-  };
-}) => {};
-
-export default validateMediaDiffsPaths;
