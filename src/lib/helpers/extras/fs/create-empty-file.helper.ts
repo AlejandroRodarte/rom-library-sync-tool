2cc7dba6db3d5203d066cc194a4b6334790f6131
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2026-08-31 11:15:31 -0600

    add: new fsExtras.createEmptyFile

---
+++ src/lib/helpers/extras/fs/create-empty-file.helper.ts
@@ -0,0 +1,13 @@
+import open, { type OpenError } from "../../wrappers/modules/fs/open.helper.js";
+
+export type CreateEmptyFileError = OpenError;
+
+const createEmptyFile = async (
+  filePath: string,
+): Promise<CreateEmptyFileError | undefined> => {
+  const [openFileHandle, openFileError] = await open(filePath, "r");
+  if (openFileError) return openFileError;
+  await openFileHandle.close();
+};
+
+export default createEmptyFile;
